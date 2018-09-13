df34bb9384152ea131fb7e10b19cea65ccd15782
paulbellmann <paul.bellmann@yahoo.de>
2018-09-13 10:20:07 +0200

    + more tests

--- portal/tests.py
+++ portal/tests.py
@@ -17,3 +17,25 @@ class AlgorithmTest(TestCase):
 
         self.assertEqual(right_path, [1, 3, 12, 13])
         self.assertEqual(wrong_path, 'Path not reachable')
+
+class ShowAllTest(TestCase):
+    """showing all points on the map"""
+
+    def test_show_all_map(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Burgtor')
+        self.assertContains(response, 'Kanalstraße')
+        self.assertEqual(len(response.context['graph']), 128)
+
+
+class ShowOneRoute(TestCase):
+    """showing the route from start to end"""
+
+    def test_show_show_route(self):
+        response = self.client.get('/route', {'start': 1, 'end': 13})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Burgtor')
+        self.assertContains(response, 'Große Gröpelgrube / Rosenstraße')
+        self.assertEqual(len(response.context['graph']), 4)
+        self.assertEqual(response.context['path'], [1, 3, 12, 13])
